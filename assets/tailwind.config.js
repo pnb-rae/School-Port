725c818224757e7cedd9691123562fcd715ae083
pnb-rae <ryanwaweru475@gmail.com>
2026-01-30 15:35:26 +0300

    new update

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -74,4 +74,4 @@ module.exports = {
 }
 
 
-sgsgsssss jhsjsjsjss jfjfjfjfjfjj hsjsjsjsjjs hshshsjsjs hdhdjdhgfawuhkfdsf jhsjsjsdgfhagklgfeu
+sgsgsssss jhsjsjsjss jfjfjfjfjfjj hsjsjsjsjjs hshshsjsjs hdhdjdhgfawuhkfdsf jhsjsjsdgfhagklgfeu hshshsh
