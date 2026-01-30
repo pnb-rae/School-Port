d0304aefeb99a01e17eabd86c94165f6a175260b
pnb-rae <ryanwaweru475@gmail.com>
2026-01-30 15:32:34 +0300

    new update

--- .formatter.exs
+++ .formatter.exs
@@ -4,6 +4,3 @@
   plugins: [Phoenix.LiveView.HTMLFormatter],
   inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"]
 ]
-
-
-dhdjjdjdjd hshshshhs  hhhdhdhdhdhdh hjjjdjkdjkdjd ksksksksksk hshshshhshs
